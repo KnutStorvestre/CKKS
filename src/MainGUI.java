ce6fe56be8395f770a9c0b1292b0eca44ec98f2f
KnutStorvestre <knutmathiasstorvestre@gmail.com>
2022-08-26 19:07:47 +0200

    Removed code in MainGUI

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -7,25 +7,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public class MainGUI {
 
     public static void main(String[] args) {
-        VectorSizeNumGUI a = new VectorSizeNumGUI();
-        // returns two ints
-
-        //FillInVectors b = new FillInVectors(2,2);
-        /*
-        double[][] testVectors = new double[2][4];
-        testVectors[0][0] = 3;
-        testVectors[0][1] = 3;
-        testVectors[1][0] = 2;
-        testVectors[1][1] = 2;
-
-         */
-
-        //ParametersInput a = new ParametersInput(2,2,testVectors);
-        // encoding encryption evaluation
-
-
-        // TODO add big math to maven
-
+        VectorSizeNumGUI GUI = new VectorSizeNumGUI();
     }
 
 }
